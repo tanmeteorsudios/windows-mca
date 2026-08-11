02d066acd8996102433f4ca691baf6228ef8da1c
tanmeteorsudios <tanmeteorsudios@gmail.com>
2026-08-11 22:56:47 +0300

    Update main.py, images.g.jres, images.g.ts, main.ts

--- main.py
+++ main.py
@@ -33,7 +33,15 @@ kullanicilar.set_position(9, 29)
 
 off = sprites.create(assets.image("""off"""),
     SpriteKind.player)
-off.set_position(9, 50)
+off.set_position(9, 90)
+
+youtube = sprites.create(assets.image("""youtube"""),
+    SpriteKind.player)
+youtube.set_position(9, 50)
+
+steam = sprites.create(assets.image("""steam"""),
+    SpriteKind.player)
+steam.set_position(9, 70)
 
 def a_tusu():
     if mouse.overlaps_with(yourconsole):
@@ -48,6 +56,10 @@ def a_tusu():
         pause(1000)
         sprites.destroy(off)
         pause(1000)
+        sprites.destroy(steam)
+        pause(1000)
+        sprites.destroy(youtube)
+        pause(1000)        
         sprites.destroy(kullanicilar)
         pause(1000)
         sprites.destroy(yourconsole)
@@ -56,6 +68,28 @@ def a_tusu():
         def on_forever():
             game.splash("KAPATMAK GÜVENLİ")
         forever(on_forever)
+    elif mouse.overlaps_with(youtube):
+        game.splash("RIDVAN KERVEK")
+        game.splash("GEÇMİŞTEKİ FİYATLAR GÜNÜMÜZDE OLSA(MCA)")
+        game.splash("-")
+        game.splash("RIDVAN:(YERDE KOLYE BULUR)")
+        game.splash("RIDVAN:BU NE YA")
+        game.splash("RIDVAN:(KOLYEYİ ALIR VE BAKKALA GİDER)")
+        game.splash("RIDVAN:KOLAY GELSİN SİGARA NE KADAR")
+        game.splash("BAKKALCI:100 LİRA")
+        game.splash("RIDVAN:(KOLYE YE DOKUNUR)")
+        game.splash("RIDVAN:ESKİDEN 50 LİRAYA BAKKALDAN BİRSÜRÜ ŞEY ALIRDIK")
+        game.splash("BAKKALCI:SENİN KAÇ LİRAN VAR")
+        game.splash("RIDVAN:75")
+        game.splash("BAKKALCI:O PARAYA 75 TANE SIGARA ALIRSIN")
+        game.splash("RIDVAN:DAYI SİGARA 100 LİRA DEDİN NASIL 75 TL YE 75 TANE SİGARA ALAYIM DAHA 1 SİGARA BİLE ALAMIYORUM")
+        game.splash("BAKKALCI:SİGARA 1 LİRA")
+        game.splash("RIDVAN:(İÇİNDEN:DELİ HERHALDE)")
+        game.splash("RIDVAN:SEN AL BU PARAYI BANA 75 TANE SİGARA VER")
+        game.splash("RIDVAN:(75 TL VERİR)")
+        game.splash("VE SON")
+    elif mouse.overlaps_with(steam):
+        game.splash("GİT GERÇEĞİNDEN OYUN OYNA")
 controller.A.on_event(ControllerButtonEvent.PRESSED, a_tusu)
 
 mouse = sprites.create(assets.image("""
--- main.ts
+++ main.ts
@@ -21,7 +21,11 @@ let kullanicilar = sprites.create(assets.image`
         `, SpriteKind.Player)
 kullanicilar.setPosition(9, 29)
 let off = sprites.create(assets.image`off`, SpriteKind.Player)
-off.setPosition(9, 50)
+off.setPosition(9, 90)
+let youtube = sprites.create(assets.image`youtube`, SpriteKind.Player)
+youtube.setPosition(9, 50)
+let steam = sprites.create(assets.image`steam`, SpriteKind.Player)
+steam.setPosition(9, 70)
 controller.A.onEvent(ControllerButtonEvent.Pressed, function a_tusu() {
     if (mouse.overlapsWith(yourconsole)) {
         game.splash("WİNDOWS MAKECODEACARDE")
@@ -35,6 +39,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function a_tusu() {
         pause(1000)
         sprites.destroy(off)
         pause(1000)
+        sprites.destroy(steam)
+        pause(1000)
+        sprites.destroy(youtube)
+        pause(1000)
         sprites.destroy(kullanicilar)
         pause(1000)
         sprites.destroy(yourconsole)
@@ -43,6 +51,28 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function a_tusu() {
         forever(function on_forever() {
             game.splash("KAPATMAK GÜVENLİ")
         })
+    } else if (mouse.overlapsWith(youtube)) {
+        game.splash("RIDVAN KERVEK")
+        game.splash("GEÇMİŞTEKİ FİYATLAR GÜNÜMÜZDE OLSA(MCA)")
+        game.splash("-")
+        game.splash("RIDVAN:(YERDE KOLYE BULUR)")
+        game.splash("RIDVAN:BU NE YA")
+        game.splash("RIDVAN:(KOLYEYİ ALIR VE BAKKALA GİDER)")
+        game.splash("RIDVAN:KOLAY GELSİN SİGARA NE KADAR")
+        game.splash("BAKKALCI:100 LİRA")
+        game.splash("RIDVAN:(KOLYE YE DOKUNUR)")
+        game.splash("RIDVAN:ESKİDEN 50 LİRAYA BAKKALDAN BİRSÜRÜ ŞEY ALIRDIK")
+        game.splash("BAKKALCI:SENİN KAÇ LİRAN VAR")
+        game.splash("RIDVAN:75")
+        game.splash("BAKKALCI:O PARAYA 75 TANE SIGARA ALIRSIN")
+        game.splash("RIDVAN:DAYI SİGARA 100 LİRA DEDİN NASIL 75 TL YE 75 TANE SİGARA ALAYIM DAHA 1 SİGARA BİLE ALAMIYORUM")
+        game.splash("BAKKALCI:SİGARA 1 LİRA")
+        game.splash("RIDVAN:(İÇİNDEN:DELİ HERHALDE)")
+        game.splash("RIDVAN:SEN AL BU PARAYI BANA 75 TANE SİGARA VER")
+        game.splash("RIDVAN:(75 TL VERİR)")
+        game.splash("VE SON")
+    } else if (mouse.overlapsWith(steam)) {
+        game.splash("GİT GERÇEĞİNDEN OYUN OYNA")
     }
     
 })
